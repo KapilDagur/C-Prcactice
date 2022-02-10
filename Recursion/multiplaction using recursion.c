ea3910910d18ec4f1bd89546ead167ed27767886
Kapil Dagur <95411822+Kapildagur@users.noreply.github.com>
2022-02-10 20:47:23 +0530

    Add files via upload

---
+++ Recursion/multiplaction using recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int mul(int num,int m)
+{
+	int sum; 
+	if(m == 0)
+		return 0;
+	return num + mul(num,m-1);
+};
+
+int main()
+{
+	int n1,n2;
+	printf("ENTER A NUMBER : ");
+	scanf("%d%d",&n1,&n2);
+	printf("\nENTERED VALUE : %d and %d\n",n1,n2);
+	int multi = mul(n1,n2);
+	printf("The multiplication of %d and %d is %d",n1,n2,multi);
+	return 0;
+	
+}
